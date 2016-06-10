0c162f629f12e97bc9a8120907fa53d57f0b6ae8
c-rajan <charlesrajan07@gmail.com>
2016-06-10 21:54:38 +0530

    Add files via upload

--- editor-js.js
+++ editor-js.js
@@ -157,7 +157,7 @@ $(document).ready(function(){
 			var text_small_paragraph_text='<div class="'+mm+'" contenteditable="true" style="position:absolute;top:'+y+'px;left:'+x+'px;font-size:80%"><p>\
   This is a paragraph</p></div>';
 	$("body").append(text_small_paragraph_text);
-		$("."+mm).draggable({cancel: "p"} );
+		$("."+mm).draggable({cancel: "p"});
 
 	
 	});
@@ -646,13 +646,17 @@ $(document).on('click',".text-option-edit-italics",function(){
 
 	$(document).on('click', '.box-option-edit-border',function(e){
 
+$(".box-edit-color-options").remove();
+$(".box-edit-shadow-options").remove();
+$(".box-edit-border-options").remove();
+$(".box-edit-opacity-options").remove();
 
-var text1='<div style="position:absolute;top:50px;left:10px;display:inline;">Border-Width : <input type="text" name="box_border_width" placeholder="Border width :  '+$("."+previous_clicked_classname).css("border-width")+'" class="box-option-edit-border-width-select" style="width:150px;"></div>	';
-var text2='<div style="position:absolute;top:70px;left:10px;display:inline;"><br>Border-Radius : <br><input type="text" name="box_border_top_left_radius" placeholder="Top-Left :  '+$("."+previous_clicked_classname).css("border-top-left-radius")+'" class="box-option-edit-border-radius-topleft-select" style="width:120px;margin-right:10px;">\
+var text1='<div style="position:absolute;top:50px;left:10px;display:inline;" class="box-edit-border-options">Border-Width : <input type="text" name="box_border_width" placeholder="Border width :  '+$("."+previous_clicked_classname).css("border-width")+'" class="box-option-edit-border-width-select" style="width:150px;"></div>	';
+var text2='<div style="position:absolute;top:70px;left:10px;display:inline;" class="box-edit-border-options"><br>Border-Radius : <br><input type="text" name="box_border_top_left_radius" placeholder="Top-Left :  '+$("."+previous_clicked_classname).css("border-top-left-radius")+'" class="box-option-edit-border-radius-topleft-select" style="width:120px;margin-right:10px;">\
 <input type="text" name="box_border_top_rightradius" placeholder="Top-Right :  '+$("."+previous_clicked_classname).css("border-top-right-radius")+'" class="box-option-edit-border-radius-topright-select" style="width:120px;"><br>\
 <input type="text" name="box_border_bottom_left_radius" placeholder="Bottom-left :  '+$("."+previous_clicked_classname).css("border-bottom-left-radius")+'" class="box-option-edit-border-radius-bottomleft-select" style="width:120px;margin-right:10px;">\
 <input type="text" name="box_border_bottom_right_radius" placeholder="Bottom-right :  '+$("."+previous_clicked_classname).css("border-bottom-right-radius")+'" class="box-option-edit-border-radius-bottomright-select" style="width:120px;"></div>	';
-var text3='<div style="position:absolute;top:170px;left:10px;display:inline;"><select class="box-option-edit-border-style">\
+var text3='<div style="position:absolute;top:170px;left:10px;display:inline;" class="box-edit-border-options"><select class="box-option-edit-border-style">\
   <option value="0">Solid Border</option>\
   <option value="1">Dashed Border</option>\
   <option value="2">Double Border</option>\
@@ -760,12 +764,16 @@ $(document).on('change', '.box-option-edit-border-style',function(e){
 
 	$(document).on('click', '.box-option-edit-shadow',function(e){
 
+		$(".box-edit-shadow-options").remove();
+		$(".box-edit-color-options").remove();
+		$(".box-edit-border-options").remove();
+		$(".box-edit-opacity-options").remove();
 		var result=$("."+previous_clicked_classname).css("box-shadow").split(/\s+/);
 
-		var text1='<div style="position:absolute;top:50px;left:10px;display:inline;">Shadow-Distance : <input type="text" name="box_shadow_distance" placeholder="Shadow Distance :  '+result[6]+'" class="box-option-edit-shadow-distance-select" style="width:150px;"></div>	';
-		var text2='<div style="position:absolute;top:70px;left:10px;display:inline;"><br>Shadow Displacement from centre : <br><input type="text" name="box_shadow_horizontal" placeholder="Horizontal Distannce :  '+result[3]+'" class="box-option-edit-shadow-horizontal-select" style="width:120px;margin-right:10px;">\
+		var text1='<div style="position:absolute;top:50px;left:10px;display:inline;" class="box-edit-shadow-options">Shadow-Distance : <input type="text" name="box_shadow_distance" placeholder="Shadow Distance :  '+result[6]+'" class="box-option-edit-shadow-distance-select" style="width:150px;"></div>	';
+		var text2='<div style="position:absolute;top:70px;left:10px;display:inline;" class="box-edit-shadow-options"><br>Shadow Displacement from centre : <br><input type="text" name="box_shadow_horizontal" placeholder="Horizontal Distannce :  '+result[3]+'" class="box-option-edit-shadow-horizontal-select" style="width:120px;margin-right:10px;">\
 		<input type="text" name="box_shadow_vertical" placeholder="Vertical Distance :  '+result[4]+'" class="box-option-edit-shadow-vertical-select" style="width:120px;"></div>';
-		var text3='<div style="position:absolute;top:140px;left:10px;display:inline;"><br>Shadow Blur : <input type="text" name="box_shadow_blur" placeholder="Shadow Blur :  '+result[5]+'" class="box-option-edit-shadow-blur-select" style="width:150px;"></div>	';
+		var text3='<div style="position:absolute;top:140px;left:10px;display:inline;" class="box-edit-shadow-options"><br>Shadow Blur : <input type="text" name="box_shadow_blur" placeholder="Shadow Blur :  '+result[5]+'" class="box-option-edit-shadow-blur-select" style="width:150px;"></div>	';
 
 
 		$(".box-option-edit2").append(text1,text2,text3);
@@ -835,9 +843,13 @@ $(document).on('change', '.box-option-edit-shadow-blur-select',function(e){
 
 $(document).on('click', '.box-option-edit-opacity',function(e){
 
+$(".box-edit-color-options").remove();
+$(".box-edit-shadow-options").remove();
+$(".box-edit-border-options").remove();
+		$(".box-edit-opacity-options").remove();
 
-alert($("."+previous_clicked_classname).css("opacity"));
-var text='<input type="text" name="box_opacity" placeholder="Opacity :  '+$("."+previous_clicked_classname).css("opacity")+'" class="box-option-edit-opacity-select" style="position:absolute;top:50px;width:100px;right:130px">	';
+
+var text='<input type="text" name="box_opacity" placeholder="Opacity :  '+$("."+previous_clicked_classname).css("opacity")+'" class="box-option-edit-opacity-select box-edit-opacity-options" style="position:absolute;top:50px;width:100px;right:130px">	';
 
 $(".box-option-edit2").append(text);
 e.stopPropogation();
@@ -860,8 +872,14 @@ $(document).on('change', '.box-option-edit-opacity-select',function(e){
 
 
 $(document).on('click', '.box-option-edit-color-border',function(e){
+
+$(".box-edit-shadow-options").remove();
+$(".box-edit-border-options").remove();
+$(".box-edit-opacity-options").remove();
+$(".box-edit-color-options").remove();
+
 	$(".box-option-edit2").remove();
- 	var text='<input type="color" class="box-option-edit-color-border-select" style="position:absolute;top:'+(300)+'px;right:'+(400)+'px;">';
+ 	var text='<input type="color" class="box-option-edit-color-border-select box-edit-color-options" style="position:absolute;top:'+(300)+'px;right:'+(400)+'px;">';
  	$("body").append(text);
  	e.stopPropogation();
 
@@ -884,8 +902,15 @@ $(document).on('change', '.box-option-edit-color-border-select',function(){
 
 $(document).on('click', '.box-option-edit-color-fill',function(e){
 
+
+$(".box-edit-shadow-options").remove();
+$(".box-edit-border-options").remove();
+$(".box-edit-opacity-options").remove();
+$(".box-edit-color-options").remove();
+
+
 	$(".box-option-edit2").remove();
- 	var text='<input type="color" class="box-option-edit-color-fill-select" style="position:absolute;top:'+(300)+'px;right:'+(400)+'px;">';
+ 	var text='<input type="color" class="box-option-edit-color-fill-select box-edit-color-options" style="position:absolute;top:'+(300)+'px;right:'+(400)+'px;">';
  	$("body").append(text);
  	e.stopPropogation();
 
@@ -911,6 +936,7 @@ $("."+previous_clicked_classname).css("background-color",colorvalue);
 	$(document).on('click', '.box-option-edit',function(e){
 
 	$(".box-option-button").remove();
+	$(".box-edit-color-options").remove();
 
 	var button_group='<div class="btn-group box-option-edit2 draggable" style="opacity:0.9;position:absolute;top:300px;right:'+(400)+'px;">\
   <button type="button" class="btn btn-primary box-option-edit-border " >Border</button>\
@@ -938,6 +964,8 @@ $(document).on('click', '.rectangular-box',function(e){
 
 		var forclass=($(this).closest('div').attr("class"));
 		previous_clicked_classname=forclass.substr(0,forclass.indexOf(' '));
+
+		$(".box-edit-color-options").remove();
 
 		if(!text_option_button_data){
 			text_option_button_data=true;
@@ -975,7 +1003,7 @@ e.stopPropogation();
 
     	$(".text-option-button").remove();
     	$(".text-option-edit2").remove();
-			var box='<div class="'+mm+' rectangular-box" contenteditable="true" style="position:absolute;top:'+y+'px;left:'+x+'px;height:50px;width:75px;border-style: solid;border-width: 2px;opacity:1;box-shadow: 0px 0px 0px 0px"></div>';
+			var box='<div class="'+mm+' rectangular-box" contenteditable="true" style=" position:absolute;top:'+y+'px;left:'+x+'px;height:50px;width:75px;border-style: solid;border-width: 2px;opacity:1;box-shadow: 0px 0px 0px 0px"></div>';
 	$("body").append(box);
 	$("."+mm).draggable().resizable();
 	
@@ -994,5 +1022,115 @@ e.stopPropogation();
 	}
 	});
 
+
+
+
+		$("#bg").click(function(){
+		$("#bg").change(function(){
+			$("body").css("background-color",$("#bg").val());
+		});
+	});
+		$(".bgcolor").click(function(){
+			$("#bg").trigger("click");
+		});
+
+
+		 
+    $("#fileinput1").change(function(){
+    	if(this.files && this.files[0]){
+    		var reader=new FileReader();
+    		reader.onload=imageIsLoaded1;
+    		reader.readAsDataURL(this.files[0]);
+    	}
+    });
+
+		function imageIsLoaded1(e){
+			$('body').css('background-image','url("'+e.target.result+'")');
+		};
+		$("#bgimage").click(function(){
+			$("#fileinput1").click();
+		});
+
+var x=300,y=300,mn=100;
+
+		$("#imgupload").click(function(){
+					$("#fileinput2").click();
+				});
+
+		 $("#fileinput2").click(function(){
+		 	var special=1;
+	   		 $("#fileinput2").change(function(){	
+	    			if(this.files && this.files[0] && special>0){
+	    			var reader=new FileReader();
+	    			reader.onload=function(e){
+	    				var imag='<img src="'+e.target.result+'" class="'+mn+'uploadedimage" alt="sorry!image not found" style="cursor:move;width:200px;height:200px;position:absolute;top:'+x+'px;left:'+y+'px">';
+						$("body").append(imag);
+						$("."+mn+"uploadedimage").resizable({ghost:true}).parent().draggable();
+						x=x+100;
+						y=y+100;
+						mn=mn+1;
+						special--;
+	    			};
+	    			reader.readAsDataURL(this.files[0]);
+	    			}
+	   		 });
+		});
+
+		
+$("#urlimage2submit").click(function(){
+	var userurl=prompt("Enter URl of Image","");
+	if(userurl!=null){
+	var imag='<img src="'+userurl+'" class="'+mn+'urlimage" style="cursor:move;position:absolute;top:'+y+'px;left:'+x+'px;width:100px;height:100px;">'
+	//'<img src=$("#urlimage2").val() class="'+mm+'" alt="" style="position:absolute;top:'+y+'px;left:'+x+'px;" width="50" height="50">';
+	$("body").append(imag);
+	$("."+mn+"urlimage").resizable({ghost:true}).parent().draggable();
+//$("."+mm).draggable();
+	x=x+50;
+	y=y+50;
+	mn=mn+1;
+}
+	return;
+});
+
+function  getId(url) {
+	var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+
+    if (match && match[2].length == 11) {
+        return match[2];
+    } else {
+        return 'error';
+    }
+}
+var xx=100;
+	var yy=200;
+	var mp=1000;
+
+
+$("#youtube").click(function(){
+	
+	var videourl=prompt("Enter the URL of the video","");
+	if(videourl!=null){
+	var videoid=getId(videourl);
+	var frame='<div class="'+mp+'youtube" style="position:absolute;top:'+xx+'px;left:'+yy+'px;width:420;height:315;"><iframe src="https://www.youtube.com/embed/'+videoid+'" style="width:95%;height:95%;" frameborder="0" allowfullscreen></iframe>'
+	$("body").append(frame);
+	$("."+mp+"youtube").draggable().resizable();
+xx=xx+50;
+yy=yy+50;
+mp=mp+1;
+}
+});
+$("#fb-video").click(function(){
+
+	var fbvideo=prompt("Enter the embed code you have copied","");
+	if(fbvideo!=null){
+		var dim='<div class="'+mn+'fb"></div>'
+		$("body").append(dim);
+		$("."+mn+"fb").html(fbvideo);
+	    $("."+mn+"fb").draggable().resizable();
+	    mn=mn+1;
+	}
+	return;
+});
 
 });
